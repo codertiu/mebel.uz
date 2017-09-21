2800278027eb44c626ab0e1398dea39aa0d762a3
codertiu <mrhope1995@gmail.com>
2017-09-21 23:12:17 +0500

    my first changing

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -10,6 +10,7 @@ return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'request' => [
